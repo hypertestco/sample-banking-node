192c0273546d33fa752ea913740c6dc823fd33a5
Karan Raina <karan95raina@hypertest.co>
2025-01-28 18:34:27 +0530

    conf files added
    Signed-off-by: Karan Raina <karan95raina@hypertest.co>

---
+++ .htConf.consumer.js
@@ -0,0 +1,27 @@
+const requestTypes = {
+  HTTP: 'HTTP',
+  AMQP: 'AMQP',
+};
+
+const serviceIdentifiers = require('./service-identifiers');
+
+
+module.exports = {
+  htBackendBaseUrl: 'https://demo.hypertest.co', // HyperTest backend URL (Required)
+  serviceIdentifier: serviceIdentifiers.transactionConsumer,
+  requestTypesToTest: [requestTypes.AMQP], // What kind of requests to include in the test
+  // httpCandidateUrl: 'http://localhost:12300', // HTTP URL of App under test (Optional)
+  appStartCommand: 'npm', // Command to start the app (Required)
+  appStartCommandArgs: ['run', 'start:consumer'], // App start command arguments (Required)
+  appWorkingDirectory: __dirname, // Working directory for the app (default: current working dir) (Optional)
+  appStartTimeoutSec: 20, // Timeout for the start command (default: 10) (Optional)
+  showAppLogs: true, // Whether to show app logs (default: false) (Optional)
+  testBatchSize: 50, // Number of concurrent test requests (default: 50) (Optional)
+  // testRequestsLimit: 0, // Number requests to test (Optional)
+  httpReqFiltersArr: [], // "<GET /users>", "<ANY REGEX:^/payments>" (Optional)
+  htExtraHeaders: {  // Object containing additional headers for HyperTest server requests (Optional)
+    // authorization: creds.authHeader,
+  },
+};
+
+// 581, 573, 577, 580
