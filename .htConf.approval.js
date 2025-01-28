5b21646cdc3fcee476d8062789dbfbc8c5b8a5ad
Karan Raina <karan95raina@hypertest.co>
2025-01-28 19:23:23 +0530

    replacing npm scripts with node for windows
    Signed-off-by: Karan Raina <karan95raina@hypertest.co>

--- .htConf.approval.js
+++ .htConf.approval.js
@@ -11,10 +11,10 @@ module.exports = {
   serviceIdentifier: serviceIdentifiers.approvalService,
   requestTypesToTest: [requestTypes.HTTP], // What kind of requests to include in the test
   httpCandidateUrl: 'http://localhost:12301', // HTTP URL of App under test (Optional)
-  appStartCommand: 'npm', // Command to start the app (Required)
-  appStartCommandArgs: ['run', 'start:approval'], // App start command arguments (Required)
+  appStartCommand: 'node', // Command to start the app (Required)
+  appStartCommandArgs: ['./services/approval-service.js'], // App start command arguments (Required)
   appWorkingDirectory: __dirname, // Working directory for the app (default: current working dir) (Optional)
-  appStartTimeoutSec: 20, // Timeout for the start command (default: 10) (Optional)
+  appStartTimeoutSec: 30, // Timeout for the start command (default: 10) (Optional)
   showAppLogs: true, // Whether to show app logs (default: false) (Optional)
   testBatchSize: 50, // Number of concurrent test requests (default: 50) (Optional)
   // testRequestsLimit: 0, // Number requests to test (Optional)
--- .htConf.banking.js
+++ .htConf.banking.js
@@ -11,10 +11,10 @@ module.exports = {
   serviceIdentifier: serviceIdentifiers.bankingService,
   requestTypesToTest: [requestTypes.HTTP], // What kind of requests to include in the test
   httpCandidateUrl: 'http://localhost:12300', // HTTP URL of App under test (Optional)
-  appStartCommand: 'npm', // Command to start the app (Required)
-  appStartCommandArgs: ['run', 'start:banking'], // App start command arguments (Required)
+  appStartCommand: 'node', // Command to start the app (Required)
+  appStartCommandArgs: ['./services/banking-service.js'], // App start command arguments (Required)
   appWorkingDirectory: __dirname, // Working directory for the app (default: current working dir) (Optional)
-  appStartTimeoutSec: 20, // Timeout for the start command (default: 10) (Optional)
+  appStartTimeoutSec: 30, // Timeout for the start command (default: 10) (Optional)
   showAppLogs: true, // Whether to show app logs (default: false) (Optional)
   testBatchSize: 50, // Number of concurrent test requests (default: 50) (Optional)
   // testRequestsLimit: 0, // Number requests to test (Optional)
