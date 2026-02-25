605f61ab48b474bf0200ae1bb972721b63bf6a23
shreesha1995 <shreesha.sudhakar@hypertest.co>
2026-02-25 13:00:08 +0530

    removed conflict added change

--- services/banking-service.js
+++ services/banking-service.js
@@ -58,7 +58,7 @@ fastify.get('/banking/test3', async (request, reply) => {
 
 // Onboard new customer
 fastify.post('/banking/onboard-customer', async (request, reply) => {
-  const { name, address, mobile } = request?.body;
+  const { name, address, mobile } = request.body;
   if (name.length < 3 || address.length < 5 || mobile.length < 10) {
     throw new Error('please fill required field correctly')
   }
@@ -236,7 +236,7 @@ const start = async () => {
     await fastify.listen({ port: 12300, host: 'localhost' });
     /* hypertest snippet starts */
     htSdk.markAppAsReady();
-    /* hypertest snippet ends */
+    /* hypertest snippet ends here*/
     fastify.log.info(`Server listening on ${fastify.server.address().port}`);
   } catch (err) /* istanbul ignore next */ {
     fastify.log.error(err);
