2595e9faeee475b680179df7fff4ea07bf4f2875
shreesha1995 <shreesha.sudhakar@hypertest.co>
2026-02-27 01:39:11 +0530

    refactor to introduce conflict

--- services/banking-service.js
+++ services/banking-service.js
@@ -62,7 +62,7 @@ fastify.get('/banking/test3', async (request, reply) => {
 
 // Onboard new customer
 fastify.post('/banking/onboard-customer', async (request, reply) => {
-  const { name, address, mobile } = request.body;
+  const { name, mobile, address } = request.body;
   if (name.length < 3 || address.length < 5 || mobile.length < 10) {
     throw new Error('please fill required field correctly')
   }
