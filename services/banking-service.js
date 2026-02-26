d85568fbb59a646ce79a735dc210ef360836922e
shreesha1995 <shreesha.sudhakar@hypertest.co>
2026-02-27 01:42:42 +0530

    reverting conflict

--- services/banking-service.js
+++ services/banking-service.js
@@ -62,7 +62,7 @@ fastify.get('/banking/test3', async (request, reply) => {
 
 // Onboard new customer
 fastify.post('/banking/onboard-customer', async (request, reply) => {
-  const { name, mobile, address } = request.body;
+  const { name, address, mobile  } = request.body;
   if (name.length < 3 || address.length < 5 || mobile.length < 10) {
     throw new Error('please fill required field correctly')
   }
